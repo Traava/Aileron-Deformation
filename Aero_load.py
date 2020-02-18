1d022fe0ab1bb0d48c11f34e776cc1dea561fafc
Marek Travnik <marek.travnik.98@gmail.com>
2020-02-18 20:56:39 +0100

    Added spline interpolation and polynomial matrix integration

--- Aero_load.py
+++ Aero_load.py
@@ -43,7 +43,68 @@ for i in range(Nx):
     CoPs[i]     = torque_contrib/lift_contrib                               #Distance is moment/force
 
 
+# ------- Cubic spline interpolation
 
+def interpol(f,x,M0,MN):
+    dim = len(x)
+    system_mat = np.zeros((dim,dim))            #System looks like: system_mat*M = output_vec
+    system_mat[0,0] = 1                         #to say that M0 = M0
+    system_mat[-1,-1] = 1                       #to say that MN = MN
+    output_vec = np.zeros(dim)
+    output_vec[0] = M0                          #to say that M0 = M0
+    output_vec[-1]= MN                          #to say that MN = MN
+    h=np.zeros(dim-1)
+    h[0] = x[1]-x[0]
+    for i in range(1,dim-1):                    #Check Numerical Analysis Reader page 43
+        h_prev = x[i]  - x[i-1]
+        h_i    = x[i+1]- x[i]
+        h[i]   = h_i
+        system_mat[i,i-1] = h_prev/6
+        system_mat[i,i]   = (h_prev + h_i)/3
+        system_mat[i,i+1] = h_i/6
+        output_vec[i] = (f[i+1] - f[i])/h_i - (f[i]-f[i-1])/h_prev
+
+    M = np.linalg.solve(system_mat, output_vec)
+    coef_mat = np.zeros((dim-1,4))
+    for i in range(dim-1):
+        print(h[i])
+        ai = (M[i+1]-M[i])/(6*h[i])
+        bi = M[i]/2
+        ci = (f[i+1]-f[i])/h[i]-h[i]*M[i]/3 - h[i]*M[i+1]/6
+        di = f[i]
+        Di = di - ci*x[i] + bi*(x[i])**2 -ai*(x[i])**3
+        Ci = ci - 2*x[i]*bi + 3*ai*(x[i])**2
+        Bi = bi - 3*ai*x[i]
+        Ai = ai
+        coef_mat[i] = np.array([Ai,Bi,Ci,Di])
+    return coef_mat
+
+
+
+coef_mat = interpol(q_tilde,x_coor,0,0)
+
+def polynomial(x,A,B,C,D):
+    return A*x**3 + B*x**2 + C*x+D
+
+points = []
+values = []
+n = 10
+for i in range(len(x_coor)-1):
+    h_i = x_coor[i + 1] - x_coor[i]
+    A ,B, C, D = tuple(coef_mat[i])
+    for j in range(n):
+        points.append(x_coor[i]+h_i*j/n)
+        values.append(polynomial(x_coor[i]+h_i*j/n,A,B,C,D))
+
+def int_pol(coefs_in):
+    size = len(coefs_in)
+    syst_mat = np.zeros((size+1,size))
+    for i in range(1,size+1):
+        syst_mat[i,i-1] = 1/i
+    coefs_out = np.dot(syst_mat,coefs_in)
+    return coefs_out
+
+print(int_pol(int_pol(np.array([1,1,1,1]).reshape(-1,1))))
 
 
 
