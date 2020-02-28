5975a8129a616f334cbfb472ceb6afc7cc09948a
YasserElnily <61149453+YasserElnily@users.noreply.github.com>
2020-02-28 10:22:06 +0100

    Add files via upload

--- Bending_stress.py
+++ Bending_stress.py
@@ -2,78 +2,109 @@ import numpy as np
 from variables import * 
 from math import *
 import matplotlib.pyplot as plt
+from MatrixSystem import *
+from MoI import *
 
     
-n = 20                                      #half the Number of panels the circumference is divided into, needs to be an even number
+n    = 300   #half the Number of panels the circumference is divided into, needs to be an even number
+n_sp = 100
+m    = 80    #m is the number of sections the span is divided into
+
 l_skin = sqrt((0.5*h)**2 + (Ca-0.5*h)**2)   #length of the inclined side of one side aileron, due to symmetry the length of the top and bottom part are the same
 panel_sep = (2*l_skin + pi*(0.5*h))/(2*n)   #Distance between the panels
 
-y_coord = np.array(0)
-z_coord = np.array(0)
+y_coord = np.array([])
+z_coord = np.array([])
 
 ## still add panel coordinates opposite side
 #coordinates with respect to the leading edge for half of the circumference
-for i in range(1,n+1):
+for i in range(n):
     if i*panel_sep <= h*pi/4:                       #panel is on the arc part of the circumference
         theta_sep = i*panel_sep/(h/2)               #angle of the second boom  with the z-axis, measured from the center of the semi-circular part
-        zp_i = (h/2)*(1-cos(theta_sep))
-        yp_i = (0.5*h)*sin(theta_sep)
-        np.append(y_coord,yp_i)
-        np.append(z_coord,zp_i)
-        i = i+1
+        zp_i = (h/2)*(1-cos(theta_sep)) - centroid
+        yp_i = -(0.5*h)*sin(theta_sep)
+        y_coord = np.append(y_coord,yp_i)
+        y_coord = np.append(y_coord,-yp_i)
+        z_coord = np.append(z_coord,zp_i)
+        z_coord = np.append(z_coord,zp_i)
+        
     elif i*panel_sep > h*pi/4:                      #panel is on the triangular part of the cross section
         theta_skin = atan((0.5*h)/(Ca-0.5*h))       #angle of the inclined sides with the horizontal
-        zp_i = (h/2) + (i*panel_sep - 0.5*pi*(0.5*h))*cos(theta_skin)
-        yp_i = 0.5*h - (i*panel_sep - 0.5*pi*(0.5*h))*sin(theta_skin)
-        np.append(y_coord,yp_i)
-        np.append(z_coord,zp_i)
-        i = i+1
-
-#circle equation
-z1 = np.linspace(0, 0.5*h, 50)
-y1 = np.sqrt((0.5*h)**2 - (z1-0.5*h)**2)      #top quarter
-y11 = -y1                                     #bottom quarter
+        zp_i = ((h/2) + (i*panel_sep - 0.5*pi*(0.5*h))*cos(theta_skin)) - centroid
+        yp_i = - (0.5*h - (i*panel_sep - 0.5*pi*(0.5*h))*sin(theta_skin))
+        y_coord = np.append(y_coord,yp_i)
+        y_coord = np.append(y_coord,-yp_i)
+        z_coord = np.append(z_coord,zp_i)
+        z_coord = np.append(z_coord,zp_i)
 
 
-#lines equation
-slope     = (-0.5*h)/(Ca-(0.5*h))
-intercept = -slope*Ca
-z2 = np.linspace(0.5*h, Ca, 150)
-y2 = slope*z2 + intercept                                       #top skin
-y22 = -y2                                                       #lower skin
+########SPAR########
+z_coord_sp = (h/2 - centroid)*np.ones(n_sp)
+y_coord_sp = np.zeros(n_sp)
+sep = h/n_sp
+for i in range(n_sp):
+        s = -h/2 + sep*i
+        y_coord_sp[i] = s
 
-plt.plot(z1,y1, 'k')                                            #plotting top quarter
-plt.plot(z1,y11, 'k')                                           #plotting bottom quarter
-plt.plot(z2,y2, 'k')                                            #plotting top skin
-plt.plot(z2,y22, 'k')                                           #plotting bottom skin
-plt.vlines(x=0.5*h, ymin=-0.5*h, ymax=0.5*h, linewidth = 3)     #plotting spar 
-plt.plot(z_coord,y_coord,'ro')                                  #plotting panels
-plt.show()
 
 
 #Bending stresses
-#from MatrixSystem import Mz(x), My(x)
-#la is span of aileron, defined in variables
-#k= np.arange(0,m,1) # where k gives the section number and m the number of sections
-#xsectionlst = np.array()
-#for i in k: # creating an array with coordinates of each section
-#    xsection = (la/m)*(i+1/2) # xcoordinates at which the moments of each cross section are evaluated are in the middle of each section
-#    np.append(xsectionlst, xsection)
-#from MoI import Izz_Aileron, Iyy_Aileron
-#sigmax = np.array(0)
-#bending_stresses = np.array(0)
-#for x in xsectionlst:          #k is number of sections along the span
-#    for i in range(1,n+1):      #i is each panel along the cross section
-#        sigmax_i = Mz[x]*y_coord[i]/Izz_Aileron + My[x] z_coord[i]/Iyy_Aileron
-#        np.append(sigmax,sigmax_i)
-#        i= i+1
-#    np.append(sigmax,bending_stresses)
-#    x=x+1
+#from MatrixSystem import Mz, My
+#Dividing the span into sections and calculating the maximum moment at each section.
+
+x_sectionstart = np.array([0])
+for j in range(1,m,1): #defining start of each section
+    x_sectionstart = np.append(x_sectionstart,j*(la/m))  #la is span of aileron, defined in variables
+    #j = j+1
+x_sectionend =  np.array([la/m])
+for k in range(2,m+1,1):#defining end of each section
+    x_sectionend = np.append(x_sectionend,(k+1)*(la/m)) 
+    #k=k+1
+#Calculate the maximum moment at each section as this will give maximum/critical stress
+#Mz_section = np.array([]) #making an empty array to which the maximum moment at each section will be appended
+#My_section = np.array([]) #making an empty array to which the maximum moment at each section will be appended
+
+#for l in range(0,m):
+    
+steps = (x_sectionstart + x_sectionend)/2
+
+M_z = Mz(steps)
+M_y = My(steps)
+
+Stress = np.zeros((m,2*n))
+for i in range(m):
+    for j in range(2*n):
+        sigma = M_z[i]*y_coord[j]/Izz_Aileron + M_y[i]*z_coord[j]/Iyy_Aileron
+        Stress[i][j] = sigma
+
+
+Stress_sp = np.zeros((m,n_sp))
+for i in range(m):
+    for j in range(n_sp):
+        sigma_sp = M_z[i]*y_coord_sp[j]/Izz_Aileron + M_y[i]*z_coord_sp[j]/Iyy_Aileron
+        Stress_sp[i][j] = sigma_sp
+
+
+print('x =', (la/m)*(20-1)+0.5*sep)
+print(np.max(Stress))
+print(np.max(Stress_sp))
 
 
 
+"""
+y_coord = np.hstack((y_coord,y_coord_sp))
+z_coord = np.hstack((z_coord,y_coord_sp))
+Stress = np.hstack((Stress[40],Stress_sp[40]))
+"""
+fig1 = plt.figure()
+ax1 = plt.axes()
+ax1.invert_yaxis()
+img1 = plt.scatter(z_coord, y_coord, c = Stress[40], cmap ='RdYlGn_r')
+#img2 = plt.scatter(z_coord_sp, y_coord_sp, c = Stress_sp[20], cmap ='RdYlGn_r') 
 
-
-
-
+cbar1 = fig1.colorbar(img1)
+cbar1.set_label(r'[$\frac{N}{m^2}$]')
+ax1.set_title("Bending Stress Distribution")
+ax1.set_xlabel('z [m]')
+ax1.set_ylabel('y [m]')
 
